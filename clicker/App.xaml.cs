439a33d0962c4f2d05ccede58868d43b4561c5aa
artxm <artem.artusevskij01@gmail.com>
2024-08-03 03:05:46 +0300

    save

--- clicker/App.xaml.cs
+++ clicker/App.xaml.cs
@@ -1,14 +1,14 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
-namespace clicker
+namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
-
 }
